bf1e203d5fed62f0fc99f6f30f3d95edb59d45a3
KDragon <elkarouani@gmail.com>
2019-05-18 02:03:38 +0000

    Task: using open trivia database to get random questions and answers

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 		<b-container class="bv-example-row">
 			<b-row>
 				<b-col sm="6" offset="3">
-					<QuestionBox />
+					<QuestionBox :question = "questions[index]" />
 				</b-col>
 			</b-row>
 		</b-container>
@@ -16,21 +16,32 @@ import Header from './components/Header.vue'
 import QuestionBox from './components/QuestionBox.vue'
 
 export default {
-  name: 'app',
-  components: {
-    Header,
-    QuestionBox
-  }
+  	name: 'app',
+  	data() {
+	  	return {
+        questions: [],
+        index: 0
+	  	}
+  	},
+  	components: {
+    	Header,
+    	QuestionBox
+  	},
+  	mounted: function() {
+		fetch('https://opentdb.com/api.php?amount=10&category=31&type=multiple', {method: 'get'})
+		.then((response) => {return response.json()})
+		.then((jsonData) => {this.questions = jsonData.results})
+  	}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  	-webkit-font-smoothing: antialiased;
+  	-moz-osx-font-smoothing: grayscale;
+  	text-align: center;
+  	color: #2c3e50;
+  	margin-top: 60px;
 }
 </style>
