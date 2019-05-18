26964ff7e8cd28af204c31c41b1541f1565d0d74
KDragon <elkarouani@gmail.com>
2019-05-18 03:12:40 +0000

    Task: adding selecting answers method

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -7,9 +7,15 @@
 
             <hr class="my-4">
 
-            <p v-for="(answer, index) in answers" :key="index">
-                {{answer}}
-            </p>
+            <b-list-group>
+                <b-list-group-item 
+                  v-for="(answer, index) in answers" 
+                  :key="index"
+                  :class="[selectedIndex === index ? 'selected' : '']" 
+                  @click="selectAnswer(index)">
+                    {{answer}}
+                </b-list-group-item>
+            </b-list-group>
 
             <b-button variant="primary" href="#">Submit</b-button>
             <b-button @click="next" variant="success" href="#">Next</b-button>
@@ -22,6 +28,16 @@ export default {
         currentQuestion: Object,
         next: Function
     },
+    data() {
+        return {
+            selectedIndex: null
+        }
+    },
+    methods: {
+        selectAnswer: function(index) {
+            this.selectedIndex = index;
+        }
+    },
     computed: {
         answers: function() {
             let answers = [...this.currentQuestion.incorrect_answers]
@@ -31,3 +47,11 @@ export default {
     }
 }
 </script>
+<style>
+    .list-group {margin-bottom: 15px;}
+    .list-group-item:hover {background-color: #EEE; cursor: pointer;}
+    .btn {margin: 0 5px;}
+    .selected {background-color: blue;}
+    .correct {background-color: green;}
+    .incorrect {background-color: red;}
+</style>
