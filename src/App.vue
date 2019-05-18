9e47801913eaf3ecc4947a9f642feaebcc07b2f0
KDragon <elkarouani@gmail.com>
2019-05-18 01:32:41 +0000

    Task: editing style on questions box component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <div id="app">
-    <Header />
-    <QuestionBox />
-  </div>
+	<div id="app">
+		<Header />
+		<b-container class="bv-example-row">
+			<b-row>
+				<b-col sm="6" offset="3">
+					<QuestionBox />
+				</b-col>
+			</b-row>
+		</b-container>
+	</div>
 </template>
 
 <script>
